25efb0a67ebcc3bd05c5357711a63816b5903b13
hjy <184638634@qq.com>
2024-04-12 19:41:47 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import add from '../components/add.vue'
-import chioceList from '../components/chioceList.vue'
-import pub from '../components/PUB.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import add from "../components/add.vue";
+import chioceList from "../components/chioceList.vue";
+import chioceList2 from "../components/chioceList2.vue";
+import pub from "../components/PUB.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/add', name: 'add', component: add },
-    { path: '/chioceList', name: 'chioceList', component: chioceList },
-    { path: '/pub', name: 'pub', component: pub }
+    { path: "/add", name: "add", component: add },
+    { path: "/chioceList", name: "chioceList", component: chioceList },
+    { path: "/chioceList2", name: "chioceList2", component: chioceList2 },
+    { path: "/pub", name: "pub", component: pub }
   ]
-})
+});
